96c6dc522e6f9b221874f97224767362b039d640
radded <mycooljames@hotmail.com>
2025-10-09 18:40:33 -0400

    number of pairs for data-render. Will probably remove this later

--- data_render/data-render.py
+++ data_render/data-render.py
@@ -5,6 +5,7 @@ import csv
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generates images of cubes given a dataset.")
     parser.add_argument('-s', '--sample-amount', type=float, default="0.33", help="The % of data that is converted into image. Defaults to 0.33%")
+    parser.add_argument('-c', '--num-pairs', type=int, default=-1 help="The number of pairs of polycubes in the dataset. This number can be retrieved automatically, but may take a long time for large datasets.")
     
     args = parser.parse_args()
     sample_amount = args.sample_amount
@@ -13,7 +14,6 @@ if __name__ == "__main__":
     try:
         with open("../data_generate/paired_cubes.csv", "r") as dataset:
             print("Opened dataset successfully!")
-            last_row = 
             print(f"Number of rows in the CSV file: {row_count}")
     except FileNotFoundError:
         print("File not found! Please make sure you generate a dataset with data_generate/data-generate.py")
